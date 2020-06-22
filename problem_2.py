717cfaac0c4745bc5fb735e4f6f2cab89bce4c91
Juan Garcia Bonilla <juan@garciabonilla.com>
2020-06-22 11:46:14 +0200

    Problem 2 Initial commit

---
+++ problem_2.py
@@ -0,0 +1,36 @@
+import os
+
+def find_files(suffix, path):
+    """
+    Find all files beneath path with file name suffix.
+
+    Note that a path may contain further subdirectories
+    and those subdirectories may also contain further subdirectories.
+
+    There are no limit to the depth of the subdirectories can be.
+
+    Args:
+      suffix(str): suffix if the file name to be found
+      path(str): path of the file system
+
+    Returns:
+       a list of paths
+    """
+
+    sol = []
+    for fil in os.listdir(path):
+
+        if os.path.isfile(path + "/" + fil):
+            if fil.endswith(suffix):
+                sol.append(path + "/" + fil)
+        else:
+            sol = sol + find_files(suffix, path + "/" + fil)
+
+    return sol
+
+def test_find_files():
+
+    assert(find_files(".c", "./testdir_p_2") == ['./testdir_p_2/subdir1/a.c', './testdir_p_2/subdir3/subsubdir1/b.c', './testdir_p_2/subdir5/a.c', './testdir_p_2/t1.c'])
+    assert(find_files(".gitkeep", "./testdir_p_2") == ['./testdir_p_2/subdir2/.gitkeep', './testdir_p_2/subdir4/.gitkeep'])
+
+test_find_files()
