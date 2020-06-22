5a9bc9ad2ec11c2f2b809cbf342a4a51a3006ae6
Juan Garcia Bonilla <juan@garciabonilla.com>
2020-06-22 15:50:34 +0200

    Added main body to demonstrate functionality

--- problem_3.py
+++ problem_3.py
@@ -1,3 +1,5 @@
+import sys
+
 """
 Implementation of a sorted queue using a binary heap.
 Enqueue and dequeue operations take O(log n) time.
@@ -175,4 +177,21 @@ def test_methods():
     test_get_huffman_codes()
     test_huffman_encoding()
 
-test_methods()
+if __name__ == "__main__":
+    test_methods()
+    codes = {}
+
+    a_great_sentence = "The bird is the word"
+
+    print ("The size of the data is: {}\n".format(sys.getsizeof(a_great_sentence)))
+    print ("The content of the data is: {}\n".format(a_great_sentence))
+
+    encoded_data, tree = huffman_encoding(a_great_sentence)
+
+    print ("The size of the encoded data is: {}\n".format(sys.getsizeof(int(encoded_data, base=2))))
+    print ("The content of the encoded data is: {}\n".format(encoded_data))
+
+    decoded_data = huffman_decoding(encoded_data, tree)
+
+    print ("The size of the decoded data is: {}\n".format(sys.getsizeof(decoded_data)))
+    print ("The content of the encoded data is: {}\n".format(decoded_data))
